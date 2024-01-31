0a7c3619d00a2297fa71ce68da3d49af07b2bbb7
Olivier Pepin <olivier.pepin@mrm.com>
2024-01-31 15:52:27 -0500

    komunicate

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './aem.js';
+import {initKomunicate} from './komumicate.js';
 
 
 (function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/OvZJjazGAYatukFYsAzrYaxZFi29K1nq1UXolTok/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
@@ -7,5 +8,7 @@ import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+
+initKomunicate(document, window.kommunicate || {});
 
 // add more delayed functionality here
---
+++ scripts/komumicate.js
@@ -0,0 +1,8 @@
+export function initKomunicate(d, m){
+    var kommunicateSettings = 
+        {"appId":"85098beeb061d4e75d49f520213f8cd","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+}
